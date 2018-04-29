06e34b85daffd94316c1efbca77478903e94ab5d
gabriel.queiroz <gabriel.fatec@hotmail.com>
2018-04-29 00:26:31 -0300

    Setup postgresql database and create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def create
+    params.require(:user).permit!
+
+    @user = User.new(params[:user])
+    @user.save
+
+    redirect_to '/'
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
 
+      t.timestamps
+    end
+  end
 end
