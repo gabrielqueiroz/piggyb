bec52824f8388a7037173692ecf775613cb0ed6f
gabriel.queiroz <gabriel.fatec@hotmail.com>
2018-04-30 14:41:27 -0300

    Add basic auth athentication on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { message: exception.message, params: [exception.param] }, status: :bad_request
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { message: exception.message }, status: :not_found
+  end
+
+  def current_user
+    case request.format
+    when Mime[:json]
+      @user ||= authenticate_user
+    else
+      @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+
+  private
+  def authenticate_user
+    authenticate_with_http_basic do |email, password|
+      user = User.find_by!(email: email)
+      return user if user.present? && user.authenticate(password)
+    end
+  end
 end
