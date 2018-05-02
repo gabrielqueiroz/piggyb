cfbbf1b95ad489f4d89fcba802dc0ffa57e814b4
gabriel.queiroz <gabriel.fatec@hotmail.com>
2018-05-02 15:29:33 -0300

    Add tests for logins controller

--- spec/controllers/logins_controller_spec.rb
+++ spec/controllers/logins_controller_spec.rb
@@ -1,5 +1,40 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe LoginsController, type: :controller do
+describe LoginsController do
 
+  context "POST /logins" do
+    let(:user) { create(:user, :random_email) }
+
+    before do
+      post :create, params: params, format: :html
+    end
+
+    context "when user exist in our databse" do
+      let(:params) do
+        {
+          login: {
+            email: user.email,
+            password: user.password
+          }
+        }
+      end
+
+      it { expect(session[:user_id]).not_to be_nil }
+      it { expect(response).to redirect_to piggy_banks_path }
+    end
+
+    context "when user does not in our databse" do
+      let(:params) do
+        {
+          login: {
+            email: 'user.not.found@test.com',
+            password: 'test'
+          }
+        }
+      end
+
+      it { expect(session[:user_id]).to be_nil }
+      it { expect(response).to redirect_to root_path }
+    end
+  end
 end
