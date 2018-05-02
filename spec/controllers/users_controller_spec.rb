307ef064bb40c9954e9a936a56488b6d91281fe7
gabriel.queiroz <gabriel.fatec@hotmail.com>
2018-05-02 15:23:48 -0300

    Add test for create user when format is html

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,74 +6,98 @@ describe UsersController do
 
   describe "POST /users" do
     before do
-      post :create, params: params, format: :json
+      post :create, params: params, format: format
     end
 
-    context "when all necessary params are present" do
-      let(:params) do
-        {
-          user: {
-            name: "Gabriel Queiroz",
-            email: "unique.email@test.com",
-            password: "test",
-            password_confirmation: "test"
+    context "using JSON format" do
+      let(:format) { :json }
+
+      context "when all necessary params are present" do
+        let(:params) do
+          {
+            user: {
+              name: "Gabriel Queiroz",
+              email: "unique.email@test.com",
+              password: "test",
+              password_confirmation: "test"
+            }
           }
-        }
+        end
+
+        it { expect(body[:name]).to eq "Gabriel Queiroz" }
+        it { expect(body[:email]).to eq "unique.email@test.com" }
+        it { expect(body[:password_digest]).to be_nil }
+        it { expect(response.status).to eq 201 }
       end
 
-      it { expect(body[:name]).to eq "Gabriel Queiroz" }
-      it { expect(body[:email]).to eq "unique.email@test.com" }
-      it { expect(body[:password_digest]).to be_nil }
-      it { expect(response.status).to eq 201 }
-    end
+      context "when params are missing" do
+        let(:params) { Hash.new }
 
-    context "when params are missing" do
-      let(:params) { Hash.new }
-
-      it { expect(body[:message]).to eq 'param is missing or the value is empty: user' }
-      it { expect(response.status).to eq 400 }
-    end
-
-    context "when email has already been taken" do
-      let(:user) { create(:user, :random_email) }
-      let(:params) do
-        { user: user.as_json }
+        it { expect(body[:message]).to eq 'param is missing or the value is empty: user' }
+        it { expect(response.status).to eq 400 }
       end
 
-      it { expect(body[:message]).to eq 'Validation failed: Email has already been taken' }
-      it { expect(response.status).to eq 400 }
-    end
+      context "when email has already been taken" do
+        let(:user) { create(:user, :random_email) }
+        let(:params) do
+          { user: user.as_json }
+        end
 
-    context "when password confirmation doesn't match password" do
-      let(:params) do
-        {
-          user: {
-            name: "Gabriel Queiroz",
-            email: "password@test.com",
-            password: "password",
-            password_confirmation: "different"
+        it { expect(body[:message]).to eq 'Validation failed: Email has already been taken' }
+        it { expect(response.status).to eq 400 }
+      end
+
+      context "when password confirmation doesn't match password" do
+        let(:params) do
+          {
+            user: {
+              name: "Gabriel Queiroz",
+              email: "password@test.com",
+              password: "password",
+              password_confirmation: "different"
+            }
           }
-        }
+        end
+
+        it { expect(body[:message]).to eq "Validation failed: Password confirmation doesn't match Password" }
+        it { expect(response.status).to eq 400 }
       end
 
-      it { expect(body[:message]).to eq "Validation failed: Password confirmation doesn't match Password" }
-      it { expect(response.status).to eq 400 }
+      context "when email is invalid" do
+        let(:params) do
+          {
+            user: {
+              name: "Gabriel Queiroz",
+              email: "email-invalid",
+              password: "test",
+              password_confirmation: "test"
+            }
+          }
+        end
+
+        it { expect(body[:message]).to eq "Validation failed: Email is invalid" }
+        it { expect(response.status).to eq 400 }
+      end
     end
 
-    context "when email is invalid" do
-      let(:params) do
-        {
-          user: {
-            name: "Gabriel Queiroz",
-            email: "email-invalid",
-            password: "test",
-            password_confirmation: "test"
-          }
-        }
-      end
+    context "using HTML format" do
+      let(:format) { :html }
 
-      it { expect(body[:message]).to eq "Validation failed: Email is invalid" }
-      it { expect(response.status).to eq 400 }
+      context "when all necessary params are present" do
+        let(:params) do
+          {
+            user: {
+              name: "Gabriel Queiroz",
+              email: "unique.email@test.com",
+              password: "test",
+              password_confirmation: "test"
+            }
+          }
+        end
+
+        it { expect(session[:user_id]).not_to be_nil }
+        it { expect(response).to redirect_to piggy_banks_path }
+      end
     end
 
   end
