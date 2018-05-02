e583672e468a5a0a3e1cd65d8d23235e74803ca4
gabriel.queiroz <gabriel.fatec@hotmail.com>
2018-05-01 21:17:49 -0300

    Refactor movements controller spec

--- spec/controllers/movements_controller_spec.rb
+++ spec/controllers/movements_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+include ActionController::HttpAuthentication::Basic
+
 describe MovementsController do
 
   context "GET /piggy_banks/:id/movements" do
@@ -9,17 +11,21 @@ describe MovementsController do
     let(:piggy_bank) { movement.piggy_bank }
     let(:user) { piggy_bank.user }
 
-    context "when there is movements for piggy bank" do
-      let(:params) { { piggy_bank_id: piggy_bank.id } }
+    before do
+      request.env['HTTP_AUTHORIZATION'] = authorization
+      get :show, format: request_format, params: params, session: session
+    end
 
-      before do
-        request.env['HTTP_AUTHORIZATION'] = authorization
-        get :show, format: request_format, params: params, session: session
+    context "when there are movements for piggy bank" do
+      let(:params) do
+        { piggy_bank_id: piggy_bank.id }
       end
 
-      context "using session" do
-        let(:session) { { user_id: user.id } }
+      context "using HTML format" do
         let(:request_format) { :html }
+        let(:session) do
+          { user_id: user.id }
+        end
 
         it { expect(subject).to render_template(:show) }
         it { expect(assigns(:movements)).not_to be_nil }
@@ -28,9 +34,10 @@ describe MovementsController do
         it { expect(assigns(:movements).first.amount).to eq -60.0 }
       end
 
-      context "using basic auth" do
-        let(:authorization) { ActionController::HttpAuthentication::Basic.encode_credentials(user.email, user.password) }
+      context "using JSON format" do
         let(:request_format) { :json }
+        let(:authorization) { encode_credentials(user.email, user.password) }
+
         let(:body) { JSON.parse(response.body, symbolize_names: true) }
 
         it { expect(response.status).to eq 200 }
